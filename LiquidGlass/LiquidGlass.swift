311b86cf21c9d0a5b8d53c4e19aae517051855f1
Jesus <was121995@gmail.com>
2026-04-19 13:47:43 +0200

    Initial Commit

---
+++ LiquidGlass/LiquidGlass.swift
@@ -0,0 +1,9 @@
+//
+//  LiquidGlass.swift
+//  LiquidGlass
+//
+//  Created by Jesús Gutiérrez López on 19/4/26.
+//
+
+import Foundation
+
---
+++ LiquidGlassTests/LiquidGlassTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassTests.swift
+//  LiquidGlassTests
+//
+//  Created by Jesús Gutiérrez López on 19/4/26.
+//
+
+import Testing
+@testable import LiquidGlass
+
+struct LiquidGlassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
